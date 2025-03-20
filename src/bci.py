e4d250cc4052486e03caec03a05bef1d36caba13
Ioanna Chourdaki <102799661+ioannachourdaki@users.noreply.github.com>
2025-03-20 19:35:52 +0200

    Update bci.py

--- src/bci.py
+++ src/bci.py
@@ -2,6 +2,7 @@ import scipy.io
 import mne
 import numpy as np
 import os
+from src.utils import apply_ica
 from tqdm import tqdm
 
 
@@ -23,36 +24,22 @@ class BCILoader:
 
     
     def preprocessing(self, file_path):
-        raw = mne.io.read_raw_gdf(file_path, preload=True, exclude='misc', verbose=False)
+        raw = mne.io.read_raw_gdf(file_path, preload=True, verbose=False)
         rename_dict = {
-                        'EEG-Fz': 'Fz', 'EEG-C3': 'C3', 'EEG-Cz': 'Cz', 'EEG-Pz': 'Pz',
-                        'EEG-0': 'FC3', 'EEG-1': 'FC1', 'EEG-2': 'FCz', 'EEG-3': 'FC2',
-                        'EEG-4': 'FC4', 'EEG-5': 'C5', 'EEG-6': 'C1', 'EEG-7': 'C2', 
-                        'EEG-8': 'C6', 'EEG-9': 'CP3', 'EEG-10': 'CP1', 'EEG-11': 'CPz',
-                        'EEG-12': 'CP2', 'EEG-13': 'CP4', 'EEG-14': 'P1', 'EEG-15': 'P2',
-                        'EEG-16': 'POz'
+                        'EEG-Fz': 'Fz', 'EEG-C3': 'C3', 'EEG-Cz': 'Cz', 'EEG-C4': 'C4', 
+                        'EEG-Pz': 'Pz','EEG-0': 'FC3', 'EEG-1': 'FC1', 'EEG-2': 'FCz', 
+                        'EEG-3': 'FC2', 'EEG-4': 'FC4', 'EEG-5': 'C5', 'EEG-6': 'C1', 
+                        'EEG-7': 'C2', 'EEG-8': 'C6', 'EEG-9': 'CP3', 'EEG-10': 'CP1', 
+                        'EEG-11': 'CPz', 'EEG-12': 'CP2', 'EEG-13': 'CP4', 'EEG-14': 'P1', 
+                        'EEG-15': 'P2', 'EEG-16': 'POz'
         }
         raw.rename_channels(rename_dict)
         raw.set_channel_types({'EOG-left': 'eog'})
         raw.set_channel_types({'EOG-central': 'eog'})
         raw.set_channel_types({'EOG-right': 'eog'})
-
-        def apply_ica(raw):
-            ica = mne.preprocessing.ICA(method='fastica', max_iter=1000, random_state=42)
-            ica.fit(raw)  
-            ica.plot_components()
-            # Find ECG artifacts
-            eog_inds, scores = ica.find_bads_eog(raw)
-            print(eog_inds)
-            # Remove identified EOG components
-            ica.exclude = eog_inds
-            # Apply ICA to clean EEG data
-            ica.apply(raw)
-            # Remove EOG channels
-            return raw.pick_types(eeg=True, verbose=False)
-        
         raw = apply_ica(raw)
-        new_file_path = file_path.replace(".gdf", "") + "_preproc.fif"
+        
+        new_file_path = file_path.replace(".gdf", "") + "_raw.fif"
         raw.save(new_file_path, overwrite=True)
         print("Preprocessed file is saved!")
         return raw
@@ -69,12 +56,12 @@ class BCILoader:
         if not self.preprocessed:
             raw = self.preprocessing(file_path)
         else:
-            raw = mne.io.read_raw_fif(file_path, preload=True)
+            raw = mne.io.read_raw_fif(file_path, preload=True, verbose=False)
 
         trim_size = self.trim_length * self.sfreq
 
         # Select channels
-        raw.pick_channels(self.selChannels)
+        raw.pick_channels(self.selChannels, verbose=False)
 
         eegs = []
 
@@ -82,15 +69,16 @@ class BCILoader:
 
         for segment in segments:
             eegs.append({'raw': mne.io.RawArray(segment, raw.info, verbose=False), 
-                        'subject': int(file_path[2])
+                        'subject': int(file_path.split("/")[-1][2])
                         })
 
         return eegs
 
     def load_dataset(self):
         all_eegs = []
-        gdf_files = [os.path.join(self.data_dir, f) for f in os.listdir(self.data_dir) if not f.endswith("E.gdf")]
-        print(gdf_files)
-        for gdf_file in tqdm(gdf_files, desc="Loading Motor-Imagery Data"):
-          all_eegs.extend(self.load_file(gdf_file))
+        files = [os.path.join(self.data_dir, f) for f in os.listdir(self.data_dir)]
+
+        for file in tqdm(files, desc="Loading Motor-Imagery Data"):
+          all_eegs.extend(self.load_file(file))
+
         return all_eegs
